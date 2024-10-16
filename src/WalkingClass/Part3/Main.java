be98aa5f501745ecdba35a8f6db9cebb0152198e
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-16 23:21:13 +0900

    241016 자바 걷기반 3번문제 완료

--- src/WalkingClass/Part3/Main.java
+++ src/WalkingClass/Part3/Main.java
@@ -1,5 +1,11 @@
 package WalkingClass.Part3;
 
 public class Main {
-
+    public static void main(String[] args) {
+     for (int i = 2; i <= 9; i++) {
+         for (int j = 1; j <= 9; j++) {
+             System.out.println(i + " X " + j + " = " + i * j);
+         }
+     }
+    }
 }
