b99f0e9dad5b658f21e0130b6f24a8f9945869a8
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-10-16 23:14:15 +0900

    241016 자바 걷기반 2번문제 완료

--- src/WalkingClass/Part2/Main.java
+++ src/WalkingClass/Part2/Main.java
@@ -1,4 +1,11 @@
 package WalkingClass.Part2;
 
 public class Main {
+    public static void main(String[] args) {
+        for (int i=1; i <= 100; i++) {
+            if (i%2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
